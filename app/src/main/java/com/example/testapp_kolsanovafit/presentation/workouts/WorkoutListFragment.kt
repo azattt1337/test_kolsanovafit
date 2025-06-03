6fff316ae830647312025be5d69c8c03a024cdee
Azat <azat.tatarin1337@gmail.com>
2025-06-03 15:15:59 +0500

    Add ViewModelFactory for viewmodels and ViewModelModule for DI, injecting factory to WorkoutListFragment

--- app/src/main/java/com/example/testapp_kolsanovafit/presentation/workouts/WorkoutListFragment.kt
+++ app/src/main/java/com/example/testapp_kolsanovafit/presentation/workouts/WorkoutListFragment.kt
@@ -1,13 +1,30 @@
 package com.example.testapp_kolsanovafit.presentation.workouts
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.testapp_kolsanovafit.R
+import com.example.testapp_kolsanovafit.WorkoutApplication
+import com.example.testapp_kolsanovafit.di.ViewModelFactory
+import javax.inject.Inject
 
 class WorkoutListFragment : Fragment() {
+
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel: WorkoutListViewModel by viewModels {
+        viewModelFactory
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as WorkoutApplication)
+            .appComponent.inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
